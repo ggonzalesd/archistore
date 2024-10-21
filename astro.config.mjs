38dc80b49f6422be1aac8452ac9047da130b02fa
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2024-10-20 20:07:01 -0500

    Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), preact()],
+  adapter: vercel(),
 });
